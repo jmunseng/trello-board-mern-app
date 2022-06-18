92b77c39adc13daf2e40524d5924083eea4d6566
munseong jeong <jmunseng@gmail.com>
2022-06-18 14:38:22 +1000

    card order done

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './Card.scss';
 const Card = (props) => {
+  const { card } = props;
   return (
     <li className="card-item">
-      <img
-        src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png"
-        alt="img1"
-      />
-      Design & Research
+      {card.image && <img src={card.image} className="card-cover" alt="" />}
+      {card.title}
     </li>
   );
 };
--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import Card from '../Card/Card';
+import { mapOrder } from '../../utilities/sorts';
 import './Column.scss';
 
 const Column = (props) => {
   const { column } = props;
+  const cards = mapOrder(column.cards, column.cardOrder, 'id');
+
   return (
     <div className="column">
       <header>{column.title}</header>
       <ul className="card-list">
-        <Card />
-        <li className="card-item">third</li>
-        <li className="card-item">second</li>
-        <li className="card-item">third</li>
-        <li className="card-item">second</li>
-        <li className="card-item">third</li>
-        <li className="card-item">second</li>
-        <li className="card-item">third</li>
-        <li className="card-item">second</li>
-        <li className="card-item">third</li>
-        <li className="card-item">second</li>
-        <li className="card-item">third</li>
-        <li className="card-item">second</li>
+        {cards &&
+          cards.length > 0 &&
+          cards.map((card, index) => {
+            return <Card key={index} card={card} />;
+          })}
       </ul>
       <footer>Add another card</footer>
     </div>
